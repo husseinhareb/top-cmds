297b105da53a006c0b1e2af2c73be1afd9a8ca8a
husseinhareb <shtam.hareb12@gmail.com>
2024-03-24 22:39:57 +0200

    history fetch

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::process::Command;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 
 const FISH_SHELL: &str = "/usr/bin/fish";
 const ZSH_SHELL: &str = "/usr/bin/zsh";
@@ -13,11 +16,11 @@ fn fetch_shell() -> String {
 
     if output.status.success() {
         let shell = String::from_utf8_lossy(&output.stdout);
-        return shell.trim().to_string();
+        shell.trim().to_string()
     } else {
         let error = String::from_utf8_lossy(&output.stderr);
         eprintln!("Error executing command:\n{}", error);
-        return String::new();
+        String::new()
     }
 }
 
@@ -27,15 +30,15 @@ fn fetch_file(shell: &str) -> String {
     match shell {
         FISH_SHELL => {
             println!("Using Fish shell");
-            file_path = "HOME/.local/share/fish/fish_history"; // Assign file path
+            file_path = ".local/share/fish/fish_history"; // Assign file path
         }
         ZSH_SHELL => {
             println!("Using Zsh shell");
-            file_path = "HOME/.zsh_history";
+            file_path = ".zsh_history";
         }
         BASH_SHELL => {
             println!("Using Bash shell");
-            file_path = "HOME/.bash_history";
+            file_path = ".bash_history";
         }
         _ => {
             println!("Unknown shell");
@@ -46,8 +49,53 @@ fn fetch_file(shell: &str) -> String {
     file_path.to_string() 
 }
 
+fn fetch_history(file_path: &str, shell: &str) -> Vec<String> {
+    let mut history = Vec::new();
+
+    if let Ok(file) = File::open(Path::new(&std::env::var("HOME").unwrap()).join(file_path)) {
+        let reader = io::BufReader::new(file);
+        let mut skip_next_line = false;
+
+        for line in reader.lines() {
+            if let Ok(command) = line {
+                if skip_next_line {
+                    skip_next_line = false;
+                    continue;
+                }
+
+                match shell {
+                    FISH_SHELL => {
+                        if command.starts_with("- cmd:") {
+                            let cleaned_command = command.chars().skip(6).collect::<String>();
+                            history.push(cleaned_command);
+                        } else if command.starts_with("  paths:") {
+                            skip_next_line = true;
+                        }
+                    }
+                    _ => {
+                        history.push(command);
+                    }
+                }
+            }
+        }
+    } else {
+        eprintln!("Failed to open history file");
+    }
+
+    history
+}
+
+
+
+
 fn main() {
     let shell = fetch_shell();
     let file_path = fetch_file(&shell);
     println!("File path: {}", file_path);
+    
+    let history = fetch_history(&file_path,&shell);
+    println!("History:");
+    for command in &history {
+        println!("{}", command);
+    }
 }
